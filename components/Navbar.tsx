021fac7f19143fbaf60bf1af7287aea73b8d090d
nonceforge <kitokira4@gmail.com>
2026-05-18 23:08:20 +0700

    Move connect button below navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,14 @@ import WalletConnectButton from "./WalletConnectButton";
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-[9999] w-full border-b border-zinc-900 bg-black/95">
-      <div className="relative mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <Link href="/" className="text-2xl font-black">
           NONCE<span className="text-lime-400">FORGE</span>
         </Link>
+      </div>
 
-        <div className="absolute right-4 top-1/2 z-[999999] -translate-y-1/2 pointer-events-auto">
-          <WalletConnectButton />
-        </div>
+      <div className="px-6 pb-4">
+        <WalletConnectButton />
       </div>
     </nav>
   );
