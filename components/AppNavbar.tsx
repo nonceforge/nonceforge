fc2e0f2bf10486d6564e2049b4b13e2e2d34e311
nonceforge <kitokira4@gmail.com>
2026-05-18 22:41:31 +0700

    Fix mobile navbar overlay

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -6,30 +6,19 @@ import WalletConnectButton from "./WalletConnectButton";
 export default function AppNavbar() {
   return (
     <nav className="sticky top-0 z-[9999] w-full border-b border-zinc-900 bg-black/95">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+      <div className="relative mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <Link href="/" className="text-2xl font-black">
           NONCE<span className="text-lime-400">FORGE</span>
         </Link>
 
         <div className="hidden gap-8 text-sm text-zinc-300 md:flex">
-          <Link href="/mining" className="hover:text-lime-400">
-            Mining
-          </Link>
-
-          <Link href="/leaderboard" className="hover:text-lime-400">
-            Leaderboard
-          </Link>
-
-          <Link href="/tokenomics" className="hover:text-lime-400">
-            Tokenomics
-          </Link>
-
-          <Link href="/docs" className="hover:text-lime-400">
-            Docs
-          </Link>
+          <Link href="/mining" className="hover:text-lime-400">Mining</Link>
+          <Link href="/leaderboard" className="hover:text-lime-400">Leaderboard</Link>
+          <Link href="/tokenomics" className="hover:text-lime-400">Tokenomics</Link>
+          <Link href="/docs" className="hover:text-lime-400">Docs</Link>
         </div>
 
-        <div className="relative z-[10000] flex min-w-[120px] justify-end">
+        <div className="absolute right-4 top-1/2 z-[99999] -translate-y-1/2 pointer-events-auto">
           <WalletConnectButton />
         </div>
       </div>
