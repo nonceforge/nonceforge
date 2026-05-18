ada06d0df5bb60ada3d89b3ba092baa5632ad18f
nonceforge <kitokira4@gmail.com>
2026-05-18 22:36:50 +0700

    Test mobile connect click

--- components/WalletConnectButton.tsx
+++ components/WalletConnectButton.tsx
@@ -1,33 +1,13 @@
 "use client";
 
-import { useAccount } from "wagmi";
-import {
-  useAccountModal,
-  useConnectModal,
-} from "@rainbow-me/rainbowkit";
-
 export default function WalletConnectButton() {
-  const { address, isConnected } = useAccount();
-  const { openConnectModal } = useConnectModal();
-  const { openAccountModal } = useAccountModal();
-
-  const shortAddress = address
-    ? `${address.slice(0, 6)}...${address.slice(-4)}`
-    : "Connect";
-
   return (
     <button
       type="button"
-      onClick={() => {
-        if (isConnected) {
-          openAccountModal?.();
-        } else {
-          openConnectModal?.();
-        }
-      }}
-      className="relative z-[9999] rounded-full border border-white/10 bg-white/5 px-4 py-2 text-sm font-semibold text-white hover:border-lime-400/30 hover:text-lime-400"
+      onClick={() => alert("Tombol Connect bisa diklik")}
+      className="relative z-[99999] rounded-full border border-white/10 bg-white/5 px-4 py-2 text-sm font-semibold text-white"
     >
-      {isConnected ? shortAddress : "Connect"}
+      Connect
     </button>
   );
 }
