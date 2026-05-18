605067b85fb9d092412b71d66f4aaa829e7e82fe
nonceforge <kitokira4@gmail.com>
2026-05-18 23:04:48 +0700

    Fix real mobile navbar wallet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,15 @@ import WalletConnectButton from "./WalletConnectButton";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 z-50 w-full border-b border-zinc-900 bg-black/70 backdrop-blur">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+    <nav className="sticky top-0 z-[9999] w-full border-b border-zinc-900 bg-black/95">
+      <div className="relative mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <Link href="/" className="text-2xl font-black">
           NONCE<span className="text-lime-400">FORGE</span>
         </Link>
 
-        <WalletConnectButton />
+        <div className="absolute right-4 top-1/2 z-[999999] -translate-y-1/2 pointer-events-auto">
+          <WalletConnectButton />
+        </div>
       </div>
     </nav>
   );
