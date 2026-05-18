63e9077a6a5d58f415bf80dbb3e1eca09262f0dc
nonceforge <kitokira4@gmail.com>
2026-05-18 22:43:46 +0700

    Test mobile touch event

--- components/WalletConnectButton.tsx
+++ components/WalletConnectButton.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 export default function WalletConnectButton() {
+  const testClick = () => {
+    alert("Connect berhasil disentuh");
+  };
+
   return (
     <button
       type="button"
-      onClick={() => alert("Tombol Connect bisa diklik")}
-      className="relative z-[99999] rounded-full border border-white/10 bg-white/5 px-4 py-2 text-sm font-semibold text-white"
+      onClick={testClick}
+      onTouchStart={testClick}
+      onPointerDown={testClick}
+      className="fixed right-4 top-4 z-[999999] rounded-full border border-lime-400/30 bg-black px-4 py-2 text-sm font-bold text-lime-400"
     >
       Connect
     </button>
