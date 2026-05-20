f4154d0a045a2dfc8604c94c4c587750dde12a66
nonceforge <kitokira4@gmail.com>
2026-05-20 20:29:59 +0700

    Show connect button in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,11 @@ import WalletConnectButton from "./WalletConnectButton";
 export default function Navbar() {
   return (
     <nav className="relative z-[9999] w-full border-b border-zinc-900 bg-black">
-      <div className="mx-auto flex max-w-7xl items-center justify-center px-4 pt-4">
-        <Link href="/" className="text-2xl font-black">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4">
+        <Link href="/" className="text-xl font-black md:text-2xl">
           NONCE<span className="text-lime-400">FORGE</span>
         </Link>
-      </div>
 
-      <div className="relative z-[10000] flex w-full justify-center px-4 py-4">
         <WalletConnectButton />
       </div>
     </nav>
